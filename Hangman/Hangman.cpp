1dfe87bf67de882db1159bf3dc74f5c2fc8aa5f9
Raghav Gupta <2001guptar@gmail.com>
2020-07-18 18:55:44 -0700

    Huge changes
    Hangman game core guess functionality complete, random word generating error fixed, few wording/user interface tweaks as well.

--- Hangman/Hangman.cpp
+++ Hangman/Hangman.cpp
@@ -6,23 +6,30 @@
 #include <fstream>
 #include <vector>
 #include <ctime>
-#include <random>
 using namespace std;
 /*
     Basic Concept: Computer picks random word, user tries to guess that word
     Rules: 6 tries MAX (head, body, 2 arms, 2 legs)
-
 */
 
 int main()
 {
-    string name;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    string name = " ";
     cout << "Hello Sir/Madam! What is your first name?" << endl;
     cin >> name;
+
+    cout << endl;
+    cout << endl;
     cout << "Nice to meet you " << name << "!" << endl;
+    
     cout << endl;
     cout << "This is a basic hangman game. A word will be generated that you have to guess." << endl
          << "You have 6 incorrect answer choices max to guess all the correct letters of the word." << endl;
+    
+    cout << endl;
     cout << endl;
     cout << "Pick your topic: " << endl
          << "- sports" << endl
@@ -32,8 +39,6 @@ int main()
     string topic;
     cin >> topic;
     string fileName = " ";
-
-    ifstream inFS;
 
     cout << endl;
     cout << "Okay so you picked: " << topic << endl;
@@ -48,6 +53,7 @@ int main()
         fileName = "cs.txt";
     }
 
+    ifstream inFS;
     inFS.open(fileName);
 
     if (!inFS.is_open())
@@ -68,14 +74,83 @@ int main()
 
     inFS.close();
 
+    //had to seed the random number to computer time using srand, rand() by itself was generating same number
+    unsigned seed = time(0);
+    srand(seed);
     int randomNumber = rand() % numberOfLines;
+    //cout << randomNumber << endl;
     string randomWord = words.at(randomNumber);
-
     //cout << randomWord << endl;
+
+    for (int unsigned i = 0; i < randomWord.size(); i++)
+    {
+        randomWord.at(i) = tolower(randomWord.at(i));
+    }
+
     cout << "Okay so the random word from that genre of " << topic << " has " << randomWord.length() << " letters." << endl;
     cout << "You have 6 incorrect answer choices before you lose." << endl;
 
-    
+    int incorrectChoices = 0;
+    char letterChoice = ' ';
+
+    //newWord is the the word the user will eventually come up with.
+    string newWord = "";
+
+    //prepopulating newWord with dashes and the size of the randomWord
+    for (int unsigned i = 0; i < randomWord.size(); i++)
+    {
+        newWord.push_back('-');
+    }
+
+    while (incorrectChoices <= 6 && newWord != randomWord)
+    {
+        cout << "Your word is this complete: " << newWord << endl;
+        cout << endl;
+        cout << "You have " << 6 - incorrectChoices << " incorrect answer choices remaining." << endl;
+
+        cout << endl;
+        cout << endl;
+
+        cout << "Please guess a letter: " << endl;
+        cin >> letterChoice;
+        letterChoice = tolower(letterChoice);
+
+        int index = randomWord.find(letterChoice);
+        if (index != string::npos)
+        {
+            for (unsigned i = 0; i < randomWord.size(); ++i)
+            {
+                if (letterChoice == randomWord.at(i))
+                {
+                    newWord.at(i) = letterChoice;
+                }
+            }
+            cout << endl;
+            cout << "You have made a correct choice!" << endl;
+            cout << endl;
+        }
+        else
+        {
+            cout << endl;
+            cout << "Sorry, you selected an invalid letter." << endl;
+            cout << endl;
+            incorrectChoices++;
+        }
+    }
+
+    if (incorrectChoices > 6)
+    {
+        cout << endl;
+        cout << endl;
+        cout << "Sorry you lost!" << endl;
+        cout << "The correct word was " << randomWord << "!" << endl;
+    }
+    else
+    {
+        cout << endl;
+        cout << endl;
+        cout << "Congrats, you guessed the correct word, which was " << randomWord << ", and WON!" << endl;
+    }
 
     return 0;
 }
